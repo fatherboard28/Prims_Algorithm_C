4f0a39d173fba9127cbdd4e08821e837d377275e
Jonathan Cunningham <fatherboard28@gmail.com>
2022-09-01 08:20:43 -0500

    Fixed getting the closest node

--- dykstras.c
+++ dykstras.c
@@ -1,25 +1,3 @@
-/*
- 1  function Dijkstra(Graph, source):
- 2      
- 3      for each vertex v in Graph.Vertices:
- 4          dist[v] ← INFINITY
- 5          prev[v] ← UNDEFINED
- 6          add v to Q
- 7      dist[source] ← 0
- 8      
- 9      while Q is not empty:
-10          u ← vertex in Q with min dist[u]
-11          remove u from Q
-12          
-13          for each neighbor v of u still in Q:
-14              alt ← dist[u] + Graph.Edges(u, v)
-15              if alt < dist[v]:
-16                  dist[v] ← alt
-17                  prev[v] ← u
-18
-19      return dist[], prev[]
-*/
-
 #include <stdio.h>
 #define UINT unsigned int
 
@@ -39,22 +17,23 @@ int main(void){
     //Size of data
     int SIZE = sizeof map[0] / sizeof map[0][0];
 
+    printf("SIZE: %d \nsizeof map[0]: %ld \nsizeof map[0][0]: %ld \n", SIZE, sizeof map[0], sizeof map[0][0]);
+
     UINT prev = 0, cur = 0;
     int y;
     for (y=0; y < SIZE; y++){
         //determine what node is closest
         UINT closest;
         {//Scope this logic for mem purposes
-            UINT weight = 9999999;
+            UINT weight = -1;
             UINT x;
             for (x=0; x<SIZE; x++){
-                if (map[cur][x] < weight){
+                if ((map[cur][x] < weight || weight == -1) && map[cur][x] != 0){
                     closest = x;
                     weight = map[cur][x];
                 }
             }
         }
-        printf("%i", closest);
         
         //set prev to cur
         //set cur to closest node
