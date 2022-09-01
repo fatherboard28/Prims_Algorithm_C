6e4c203e821f5ce90499405b156317ac240c380f
Jonathan Cunningham <fatherboard28@gmail.com>
2022-09-01 10:50:17 -0500

    First version works with one set of data

--- dykstras.c
+++ dykstras.c
@@ -3,26 +3,26 @@
 
 int main(void){
     //Adjacency matrix
-    int map[6][6] = {{ 0, 4, 2,  0, 0,  0},
-                   { 4, 0, 1,  5, 0,  0},
-                   { 2, 1, 0,  8, 10, 0},
-                   { 0, 5, 8,  0, 2,  6},
-                   { 0, 0, 10, 2, 0,  3},
-                   { 0, 0, 0,  6, 3,  0}  };
+    int map[5][5] = {{0, 4, 12, 0, 11},
+                     {4, 0, 0, 3, 7},
+                     {12, 0, 0, 2, 1},
+                     {0, 3, 2, 0, 4},
+                     {12, 7, 1, 4, 0}};
 
     //Diplay these arrays for final output
-    char a_nodes[6]   = {'A', 'B', 'C', 'D', 'E', 'F'};
-    char a_prev[6]    = {'-', '-', '-', '-', '-', '-'};
+    char a_nodes[5]   = {'A', 'B', 'C', 'D', 'E'};
+    char a_prev[5]    = {'-', '-', '-', '-', '-'};
+    UINT a_cost[5]    = {0,0,0,0,0};
 
     //Size of data
     int SIZE = sizeof map[0] / sizeof map[0][0];
 
-    printf("SIZE: %d \nsizeof map[0]: %ld \nsizeof map[0][0]: %ld \n", SIZE, sizeof map[0], sizeof map[0][0]);
-
-    UINT prev = 0, cur = 0;
+    UINT cur = 0;
     int y;
     for (y=0; y < SIZE; y++){
+
         //determine what node is closest
+        // - requires cur to be on the correct node
         UINT closest;
         {//Scope this logic for mem purposes
             UINT weight = -1;
@@ -35,8 +35,25 @@ int main(void){
             }
         }
         
-        //set prev to cur
-        //set cur to closest node
+        if (y != SIZE - 1){
+            //set prev to cur
+            a_prev[closest] = a_nodes[cur];
+
+            //set weight from cur -> closest
+            printf("map[%d][%d]\n", cur, closest);
+            a_cost[closest] = map[cur][closest];
+
+            //set cur to closest node
+            cur = closest;
+
+        }
+
+    }
+    
+    UINT x;
+    printf("     Node  |  Cost  |  Prev  |\n");
+    for (x=0; x < SIZE; x++){
+        printf("|%d|    %c   |    %d   |    %c   |\n", x, a_nodes[x], a_cost[x], a_prev[x]);
     }
 }
 
